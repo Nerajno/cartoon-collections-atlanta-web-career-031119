14cb8e8c39ae67d981188371e27846c2d87c6f27
active-object-6703 <nerajno@gmail.com>
2019-02-11 12:37:23 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -26,5 +26,5 @@ end
 def find_the_cheese# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
-  cheese_types.include?(cheddar)
+  cheese_types.include?("cheddar")
 end
